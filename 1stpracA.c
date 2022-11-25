2186abccd88e74f186dcfda15c0a28c3dcc6571d
ShivanshuPanwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2022-11-25 23:42:32 +0530

    Add files via upload

---
+++ 1stpracA.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main () 
+{
+    int num1,num2;
+
+    printf("Enter number1 :");
+    scanf("%d", &num1);
+
+    printf("Enter number2 :");
+    scanf("%d", &num2);
+
+    if (num1>num2)
+    printf("Number 1 is greater");
+    
+    else
+{
+    printf("Number 2 is greater");
+}
+    printf("\nCreated By : Shivanshu Panwar\nDate:23/11/22");
+
+    return 0;      
+}
